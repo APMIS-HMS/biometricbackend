965a1e191128ed1fd0988df3bf73bdb087bf9b62
lizzyladi <elizabeth.abbah@apmis.ng>
2018-06-19 17:41:49 +0100

    updated sms url

--- src/custom/sms-sender.js
+++ src/custom/sms-sender.js
@@ -3,8 +3,10 @@ const request = require('request');
 //const logger = require('winston');
 
 function sender(mesage, data, isScheduler) {
-  var url = 'http://portal.bulksmsnigeria.net/api/?username=apmis&password=apmis&message=' + mesage + '&sender=APMIS&mobiles=@@' + data.primaryContactPhoneNo + '@@';
-  if (isScheduler == true){
+
+  var url = 'https://api.africastalking.com/restless/send?username=apmis&Apikey=547c6a5e92e8a6c4b1f9878a8dc21bedcbd3a071b101efdc4928c912a6effc1a&to=' + data.primaryContactPhoneNo + '&message=' + mesage + '&from=APMIS';
+  //var url = 'http://portal.bulksmsnigeria.net/api/?username=apmis&password=apmis&message=' + mesage + '&sender=APMIS&mobiles=@@' + data.primaryContactPhoneNo + '@@';
+  if (isScheduler == true) {
     url = 'http://portal.bulksmsnigeria.net/api/?username=apmis&password=apmis&message=' + mesage + '&action=scheduled' + '&sender=APMIS&mobiles=@@' + data.primaryContactPhoneNo + '@@';
   }
   request.get(url, null, (error, response, body) => {
@@ -50,7 +52,7 @@ function sendScheduleAppointment(date, data) {
 module.exports = {
   sendPatientDetail(data) {
     sendPatientDetail(data);
-    console.log('Time out: ' + new Date());
+    // console.log('Time out: ' + new Date());
   },
   sendApmisId(data) {
     sendApmisId(data);
